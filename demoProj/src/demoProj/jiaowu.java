fca81d87c07ee10706935960bd884578a804743b
Hyc66 <862807018@qq.com>
2019-05-13 17:40:25 +0800

    在分支branch01修改并提交

--- demoProj/src/demoProj/jiaowu.java
+++ demoProj/src/demoProj/jiaowu.java
@@ -4,7 +4,8 @@ public class jiaowu {
 
 	public static void main(String[] args) {
 
-		System.out.println("good!");
+		//System.out.println("good!");
+		System.out.println("good branch!");
 		
 	}
 
